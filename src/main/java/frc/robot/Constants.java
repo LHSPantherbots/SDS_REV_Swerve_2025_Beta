fb11c9d52872bc08918f87938e7557fd44a0d13f
chadr03@gmail.com <chadr03@gmail.com>
2025-01-13 20:59:38 -0600

    Initial Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,145 @@
+ // Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+
+    // Chassis configuration
+    public static final double kTrackWidth = Units.inchesToMeters(26.5);
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(26.5);
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double kFrontLeftChassisAngularOffset = 0.0;
+    public static final double kFrontRightChassisAngularOffset = 0.0;
+    public static final double kBackLeftChassisAngularOffset = 0.0;
+    public static final double kBackRightChassisAngularOffset = 0.0;
+
+    // SPARK MAX CAN IDs
+    public static final int kFrontLeftDrivingCanId = 2;
+    public static final int kRearLeftDrivingCanId = 1;
+    public static final int kFrontRightDrivingCanId = 3;
+    public static final int kRearRightDrivingCanId = 4;
+
+    public static final int kFrontLeftTurningCanId = 22;
+    public static final int kRearLeftTurningCanId = 21;
+    public static final int kFrontRightTurningCanId = 23;
+    public static final int kRearRightTurningCanId = 24;
+
+    public static final boolean kGyroReversed = false;
+  }
+
+  public static final class ModuleConstants {
+    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+    // 13T, or 14T. This changes the drive speed of the module (a pinion gear with
+    // more teeth will result in a robot that drives faster).
+    //public static final int kDrivingMotorPinionTeeth = 14;
+
+    // Calculations required for driving motor conversion factors and feed forward
+    public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
+    public static final double kWheelDiameterMeters = 0.1016;
+    public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
+    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+    // teeth on the bevel pinion
+    public static final double kDrivingMotorReduction = 8.31;
+    public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
+        / kDrivingMotorReduction;
+  }
+
+  public static final class OIConstants {
+    public static final int kDriverControllerPort = 0;
+    public static final double kDriveDeadband = 0.05;
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1;
+
+    // Constraint for the motion profiled robot angle controller
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+
+  public static final class NeoMotorConstants {
+    public static final double kFreeSpeedRpm = 5676;
+  }
+
+  public static final class LEDs{
+    public static final int purple_Red = 148;
+    public static final int purple_Green = 0;
+    public static final int purple_Blue = 211;
+    public static final int purple_Hue = 282;
+    public static final int purple_Sat = 100;
+    public static final int purple_Val = 83;
+
+    public static final int yellow_Red = 255;
+    public static final int yellow_Green = 90;
+    public static final int yellow_Blue = 0;
+    public static final int yellow_Hue = 21;
+    public static final int yellow_Sat = 100;
+    public static final int yellow_Val = 100;
+
+    public static final int orange_Red = 251;
+    public static final int orange_Green = 51;
+    public static final int orange_Blue = 0;
+    public static final int orange_Hue = 12;
+    public static final int orange_Sat = 100;
+    public static final int orange_Val = 98;
+
+    public static final int red_Hue = 0;
+    public static final int red_Sat = 100;
+    public static final int red_Val = 100;
+
+    public static final int blue_Hue = 240;
+    public static final int blue_Sat = 100;
+    public static final int blue_Val = 100;
+
+    public static final int green_Hue = 240;
+    public static final int green_Sat = 100;
+    public static final int green_Val = 100;
+
+    public static final int Red = 255;
+    public static final int Green = 255;
+    public static final int Blue = 255;
+    public static final int white_Hue = 0;
+    public static final int white_Sat = 0;
+    public static final int white_Val = 100;
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Leds.java
@@ -0,0 +1,470 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.LEDs;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import frc.utils.RobotStatus;
+
+
+public class Leds extends SubsystemBase {
+  /* Creates a new leds */
+
+  //private static RobotStatus state = RobotStatus.DEFAULT;
+  //private static RobotStatus prevState = RobotStatus.DEFAULT;
+  public static int LEDstate = 0;
+  private AddressableLED m_led;
+  private AddressableLEDBuffer m_ledBuffer;
+  // Store what the last hue of the first pixel is
+  private int m_rainbowFirstPixelHue;
+
+  private int temp_Blue = 0;
+  private int temp_Green = 0;
+  private int temp_Red = 0;
+  private int blueStreakLED = 0;
+  private int numLoops = 0;
+  private int index = 0;
+  private int index1 = 1; //this varible should never be less than 1
+  private boolean hasReachedEnd = false;
+
+  public Leds() {
+    // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    m_led = new AddressableLED(0);
+
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(20); // 200 for new bot
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  @Override
+  public void periodic() {
+
+    
+  }
+
+  public void ledsOff() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, 0, 0, 0);
+    }
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void rainbow() {
+    // For every pixel
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      int hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    m_rainbowFirstPixelHue += 3;
+    // Check bounds
+    m_rainbowFirstPixelHue %= 180;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void pantherStreak() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, LEDs.purple_Red, LEDs.purple_Green, LEDs.purple_Blue);
+    }
+    for (int i = index; i < m_ledBuffer.getLength(); i += 2) {
+      m_ledBuffer.setRGB(i, LEDs.yellow_Red, LEDs.yellow_Green, LEDs.yellow_Blue);
+    }
+
+    if (numLoops % 10 == 0) {
+      index++;
+      index %= 2;
+      numLoops = 0;
+    }
+
+    numLoops++;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void purpleStreak() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, LEDs.purple_Red, LEDs.purple_Green, LEDs.purple_Blue);
+    }
+    for (int i = index; i < m_ledBuffer.getLength(); i += 2) {
+      m_ledBuffer.setRGB(i, LEDs.purple_Red / 4, LEDs.purple_Green / 4, LEDs.purple_Blue / 4);
+    }
+
+    if (numLoops % 10 == 0) {
+      index++;
+      index %= 2;
+      numLoops = 0;
+    }
+
+    numLoops++;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void purpleFlash(){
+    for(int i = 0; i < m_ledBuffer.getLength(); i++){
+      m_ledBuffer.setRGB(i, temp_Red, temp_Green, temp_Blue);
+    }
+
+    if (numLoops < 5){ //sets speed of flash
+      temp_Red = LEDs.purple_Red;
+      temp_Blue = LEDs.purple_Blue;
+      temp_Green = LEDs.purple_Green;
+    }
+    else{
+      temp_Red = 0;
+      temp_Blue = 0;
+      temp_Green = 0;
+    }
+
+    numLoops++;
+    numLoops %= 10;
+    
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void yellowFlash(){
+    for(int i = 0; i < m_ledBuffer.getLength(); i++){
+      m_ledBuffer.setRGB(i, temp_Red, temp_Green, temp_Blue);
+    }
+
+    if (numLoops < 5){ //sets speed of flash
+      temp_Red = LEDs.yellow_Red;
+      temp_Blue = LEDs.yellow_Blue;
+      temp_Green = LEDs.yellow_Green;
+    }
+    else{
+      temp_Red = 0;
+      temp_Blue = 0;
+      temp_Green = 0;
+    }
+
+    numLoops++;
+    numLoops %= 10;
+    
+    m_led.setData(m_ledBuffer);
+  }
+
+
+public void greenFlash(){
+    for(int i = 0; i < m_ledBuffer.getLength(); i++){
+      m_ledBuffer.setRGB(i,0, temp_Green, 0);
+    }
+
+    if (numLoops < 5){ //sets speed of flash
+      temp_Green = LEDs.Green;
+    }
+    else{
+      temp_Green = 0;
+    }
+
+    numLoops++;
+    numLoops %= 10;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void orangeFlash(){
+    for(int i = 0; i < m_ledBuffer.getLength(); i++){
+      m_ledBuffer.setRGB(i,temp_Red, temp_Green, temp_Blue);
+    }
+
+    if (numLoops < 10){ //sets speed of flash
+      temp_Red = LEDs.orange_Red;
+      temp_Green = LEDs.orange_Green;
+      temp_Blue = LEDs.orange_Blue;
+    }
+    else{
+      temp_Red = 0;
+      temp_Green = 0;
+      temp_Blue = 0;
+    }
+
+    numLoops++;
+    numLoops %= 20;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void yellowStreak() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i,LEDs.yellow_Red, LEDs.yellow_Green, LEDs.yellow_Blue);
+    }
+    for (int i = index; i < m_ledBuffer.getLength(); i += 2) {
+      m_ledBuffer.setRGB(i, LEDs.yellow_Red / 4, LEDs.yellow_Green / 4 , LEDs.yellow_Blue / 4);
+    }
+
+    if (numLoops % 10 == 0) {
+      index++;
+      index %= 2;
+      numLoops = 0;
+    }
+
+    numLoops++;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void red() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, LEDs.Red, 0, 0);//red
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void blue() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, 0, 0, LEDs.Blue);//blue
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void green() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for green
+      m_ledBuffer.setRGB(i, 0, LEDs.Green, 0);
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void yellow() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for yellow
+      m_ledBuffer.setRGB(i, LEDs.yellow_Red, LEDs.yellow_Green, LEDs.yellow_Blue);
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void orange() {
+
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for orange
+      m_ledBuffer.setRGB(i, LEDs.orange_Red, LEDs.orange_Green, LEDs.orange_Blue);
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+
+  public void purple() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for purple
+      m_ledBuffer.setRGB(i, LEDs.purple_Red, LEDs.purple_Blue, LEDs.purple_Green);
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void white() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for white
+      m_ledBuffer.setRGB(i, LEDs.Red, LEDs.Green, LEDs.Blue);
+    }
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void bluePulse() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for blue
+      m_ledBuffer.setRGB(i, 0, 0, temp_Blue);
+    }
+
+    // increase brightness
+    temp_Blue += 5;
+
+    // Check bounds
+    temp_Blue %= 255;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void greenPulse() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for blue
+      m_ledBuffer.setRGB(i, 0, temp_Green, 0);
+    }
+
+    // increase brightness
+    temp_Green += 5;
+
+    // Check bounds
+    temp_Green %= 255;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void orangePulse() {
+
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for orange
+      m_ledBuffer.setRGB(i, temp_Red, temp_Green, 0);
+    }
+
+    // increase brightness
+    temp_Red += 5;
+    temp_Green += 1;
+
+    // Check bounds
+    temp_Red %= 255;
+    temp_Green %= 51;
+
+    m_led.setData(m_ledBuffer);
+  }
+
+  public void blueStreak() {
+    for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for blue
+      m_ledBuffer.setRGB(i, 0, 0, LEDs.Blue);
+    }
+
+    // turns one led off
+    m_ledBuffer.setRGB(blueStreakLED, 0, 0, 0);
+
+    // increase brightness
+    if (numLoops % 3 == 0) {
+      blueStreakLED += 1;
+
+      // Check bounds
+      blueStreakLED %= m_ledBuffer.getLength();
+    }
+
+    m_led.setData(m_ledBuffer);
+
+    numLoops += 1;
+
+  }
+
+public void purpleStreak10() {
+    for (int i = index1; i < index1 + 10; i++) {
+      m_ledBuffer.setRGB(i, LEDs.purple_Red, LEDs.purple_Green, LEDs.purple_Blue);
+    }
+
+    m_ledBuffer.setRGB(index1 - 1, 0,0,0);
+
+    // increase brightness
+    if (numLoops % 7 == 0) {
+      index1++;
+      numLoops = 0;
+
+      // Check bounds
+      index1 = index1 % (m_ledBuffer.getLength() - 20) + 1;
+    }
+
+    m_led.setData(m_ledBuffer);
+
+    numLoops++;
+
+  }
+
+  public void purpleStreak10Return() {
+    for (int i = index1; i < index1 + 10; i++) {
+      m_ledBuffer.setRGB(i, LEDs.purple_Red, LEDs.purple_Green, LEDs.purple_Blue);
+    }
+
+    m_ledBuffer.setRGB(index1 - 1, 0,0,0);
+
+    if (numLoops % 7 == 0) {
+      numLoops = 0;
+
+      if (hasReachedEnd){
+        index1--;
+      }
+      else{
+        index1++;
+      }
+      
+      if (index1 == m_ledBuffer.getLength() - 20){
+        hasReachedEnd = true;
+      }
+      else if(index1 == 1){
+        hasReachedEnd = false;
+      }
+
+    }
+
+    if (hasReachedEnd){
+      index1--;
+    }
+
+    m_led.setData(m_ledBuffer);
+
+    numLoops++;
+
+  }
+
+  public void PurpleWhiteStrobe(){
+    if (numLoops == 0 || numLoops == 2){
+      for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, 0, 0, 0); //black
+      }
+    }
+    else if (numLoops == 1){
+      for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, LEDs.Red, LEDs.Green, LEDs.Blue);
+      }
+    }
+    else{
+      for (int i = 0; i < m_ledBuffer.getLength(); i++) {
+      m_ledBuffer.setRGB(i, LEDs.purple_Red, LEDs.purple_Blue, LEDs.purple_Green);
+      }
+    }
+
+    numLoops++;
+    numLoops %= 4;
+    m_led.setData(m_ledBuffer);
+  }
+
+
+
+
+//   public void setRobotStatus(RobotStatus newState){
+//     this.prevState = this.state;
+//     this.state = newState;
+//   }
+
+//   public RobotStatus getRobotStatus(){
+//     return this.state;
+//   }
+
+//   public RobotStatus getPrevRobotStatus(){
+//     return this.prevState;
+//   }
+
+//   public void ledState(){
+        
+//     switch(this.state){
+        
+//       case CLIMB: red(); break;
+//       case TARGET_LOCK: white(); break;
+//       case LAUNCH: blue(); break;
+//       case NOTE_STORED: orangePulse(); break;
+//       case INTAKE: if(RobotContainer.feeder.isNoteDetected()){orangeFlash();break;} greenPulse(); break;
+//       //case 1: purpleFlash(); break;
+//       //case 2: yellowFlash(); break;
+//       //case 3: purpleStreak10(); break;
+//       //case 4: yellowStreak(); break;
+//       //case 5: red(); break;
+//       case ROBOT_CENTRIC: rainbow(); break;
+//       case DEFAULT: if(RobotContainer.feeder.isNoteDetected()){orange();break;} pantherStreak(); break;
+//     }
+// }
+
+
+} 
