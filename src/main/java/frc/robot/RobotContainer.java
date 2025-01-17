88feca755d720ddc6083cd678a337f739f996614
KiahV <kiahley.m.vann@gmail.com>
2025-01-16 19:22:10 -0600

    Added climb with buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.List;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -13,17 +15,19 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import java.util.List;
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -34,9 +38,11 @@ import java.util.List;
 public class RobotContainer {
   // The robot's subsystems
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+  private final Climber climber = new Climber();
+
 
   // The driver's controller
-  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  CommandXboxController m_driverController = new CommandXboxController(OIConstants.kDriverControllerPort);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -44,6 +50,8 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+    
 
     // Configure default commands
     m_robotDrive.setDefaultCommand(
@@ -56,6 +64,9 @@ public class RobotContainer {
                 -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
                 true),
             m_robotDrive));
+            //set default command of climber 
+    climber.setDefaultCommand(new RunCommand(() -> climber.climbStop(), climber));
+
   }
 
   /**
@@ -68,10 +79,12 @@ public class RobotContainer {
    * {@link JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(m_driverController, Button.kR1.value)
-        .whileTrue(new RunCommand(
-            () -> m_robotDrive.setX(),
-            m_robotDrive));
+            m_driverController.rightTrigger().whileTrue(new RunCommand(
+                () -> m_robotDrive.setX(),
+                m_robotDrive));
+            m_driverController.leftBumper().whileTrue(new RunCommand(() -> climber.climbDown(), climber)).onFalse(new RunCommand(() -> climber.climbStop(), climber));
+           // m_driverController.rightBumper().whileTrue(new RunCommand(() -> Climber.ClimbUp()));
+
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,9 +6,10 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbConstants;
 
-public class Climber {
+public class Climber extends SubsystemBase {
     
      private final SparkMax m_Climber;
      private final SparkMaxConfig c_Climber;
@@ -25,15 +26,15 @@ public class Climber {
             .smartCurrentLimit(30);
     }
 
-    public void ClimbDown() {
+    public void climbDown() {
         m_Climber.set(-.6);
     }
 
-    public void ClimbUp() {
+    public void climbUp() {
         m_Climber.set(.6);
     }
 
-    public void ClimbStop() {
+    public void climbStop() {
         m_Climber.set(0);
     }
 }
